1204a9f22d6cada890869a724871f83f85b6dd7a
Daniyar.Mashayev <Daniyar.Mashayev@kcell.kz>
2025-12-16 23:47:30 +0500

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,13 +21,12 @@ val hibernateVersion = "6.4.0.Final"
 val h2Version = "2.2.224"
 val junitJupiterVersion = "5.10.1"
 val slf4jVersion = "2.0.9"
-val logbackVersion = "1.4.11"
 
 dependencies {
     // Provided dependencies
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
     compileOnly("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.0")
-    compileOnly("jakarta.servlet.jsp:jakarta.servlet.jsp-api:3.1.1")
+    compileOnly("jakarta.servlet.jsp:jakarta.servlet.jsp-api:4.0.0")
 
     // Runtime dependencies
     runtimeOnly("org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1")
@@ -37,8 +36,10 @@ dependencies {
     implementation("com.h2database:h2:$h2Version")
 
     // Logging
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
     // For JSP and EL
     implementation("org.glassfish:jakarta.el:4.0.2")
@@ -61,18 +62,95 @@ gretty {
     contextPath = "/"
     servletContainer = "tomcat10"
     integrationTestTask = "test"
+
+    jvmArgs = listOf(
+        "--add-opens=java.base/java.io=ALL-UNNAMED",
+        "--add-opens=java.base/java.lang=ALL-UNNAMED",
+        "--add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
+    )
+}
+
+tasks.build {
+    group = "build"
+    description = "Project compilation"
+
 }
 
 tasks.war {
     archiveFileName.set("${project.name}.war")
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
     from("src/main/webapp")
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
+    manifest {
+        attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
+    }
+
+    doLast {
+        println("WAR create: ${archiveFile.get()}")
+    }
 }
 
 tasks.register<Copy>("deployToTomcat") {
-    from(tasks.war)
-    into("/opt/tomcat/tomcat/webapps")
-    description = "Copies WAR file to external Tomcat webapps directory"
-    doNotTrackState("Destination directory has restricted access")
+    group = "deployment"
+    dependsOn("build")
+    doNotTrackState("External Tomcat directory")
+
+    val tomcatPath = "/home/daniyar/tomcat/apache-tomcat-10.1.46"
+    val webappsDir = "$tomcatPath/webapps"
+    val appName = project.name
+    val warFile = tasks.war.get().archiveFile.get().asFile
+
+    doFirst {
+
+        val appDir = File("$webappsDir/$appName")
+        if (appDir.exists()) {
+            project.delete(appDir)
+        }
+
+        val dir = File(webappsDir)
+        if (!dir.exists()) {
+            dir.mkdirs()
+        }
+    }
+
+    from(warFile)
+    into(webappsDir)
+
+}
+
+tasks.register<Exec>("appLaunch") {
+    group = "deployment"
+    dependsOn( "deployToTomcat")
+
+    val tomcatPath = "/home/daniyar/tomcat/apache-tomcat-10.1.46"
+    val appName = project.name
+
+    doFirst {
+        println("Unpacking $appName.war...")
+    }
+
+    executable = "zsh"
+    args("-c", """
+        cd $tomcatPath/webapps
+        
+        # Tomcat stop
+        ../bin/shutdown.sh 2>/dev/null || print "Tomcat has already been stopped"
+        sleep 2
+        
+        rm -rf $appName
+        
+        unzip -q -o $appName.war -d $appName/
+        
+        # Tomcat start
+        ../bin/startup.sh
+        
+    """)
+
+    doLast {
+        println("Available at: http://localhost:8080/$appName/")
+    }
 }
